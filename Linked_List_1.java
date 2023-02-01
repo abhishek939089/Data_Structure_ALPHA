161268b9016abb5b060a719d0454133697d40d8b
Abhishek Sharma <100137322+abhishek939089@users.noreply.github.com>
2023-02-01 22:28:50 +0530

    Add files via upload

--- Linked_List_1.java
+++ Linked_List_1.java
@@ -36,22 +36,35 @@ public class Linked_List_1{
         tail=newNode;
     }
 
-    public void add(int idx,int data){
-        if(idx == 0){
-            addFirst(data);
-            return;
-        }
-        Node newNode =new Node(data);
-        Node temp=head;
-        int i=0;
-        while(i<idx-1){
-            temp=temp.next;
-            i++;
-        }
-        // i=idx-1; temp->prev
-        newNode.next=temp.next;
-        temp.next=newNode;
+    // public void add(int idx,int data){
+    //     if(idx == 0){
+    //         addFirst(data);
+    //         return;
+    //     }
+    //     Node newNode =new Node(data);
+    //     Node temp=head;
+    //     int i=0;
+    //     while(i<idx){
+    //         temp=temp.next;
+    //         i++;
+    //     }
+    //     // i=idx-1; temp->prev
+    //     newNode.next=temp.next;
+    //     temp.next=newNode;
+        
+    // }
 
+    public void Reverse(){
+        Node prev=null;
+        Node curr=tail=head;
+        Node next;
+        while(curr!=null){
+            next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+        head=prev;
     }
     public void print() { //O(n)
         if(head == null){
@@ -73,7 +86,9 @@ public class Linked_List_1{
         ll.addFirst(1);
         ll.addLast(3);
         ll.addLast(4);
-        ll.add(2,9);
+        // ll.add(2,9);
+        ll.print();
+        ll.Reverse();
         ll.print();
 
     }
