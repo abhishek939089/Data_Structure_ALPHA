6c3af11758740fcf3b8b3669e1d98ad000c2f01b
Abhishek Sharma <100137322+abhishek939089@users.noreply.github.com>
2023-02-09 18:52:37 +0530

    Add files via upload

---
+++ Stock_Span_Problem.java
@@ -0,0 +1,68 @@
+// import java.util.*;
+// public class Stock_Span_Problem {
+//     public static void stockspan(int stock[], int span[]){
+//         Stack<Integer> s = new Stack<>();
+//         span[0] = 1;
+//         s.push(0);
+
+//         for(int i=1; i<stock.length; i++){
+//             int currPrice = stock[i];
+//             while(!s.isEmpty() && currPrice > stock[s.peek()]) {
+//                 s.pop();
+//             }
+//             if(s.isEmpty()){
+//                 span[i] = i+1;
+
+//             }else{
+//                 int prevHigh = s.peek();
+//                 span[i] = i - prevHigh;
+
+//             }
+//             s.push(i);
+//         }
+//     }
+//     public static void main(String args[]){
+//         int stock[] = {100, 80, 60, 70, 60, 85, 100};
+//         int span[] = new int[stock.length];
+//         stockspan(stock, span);
+
+//         for(int i=0; i<span.length; i++){
+//             System.out.println(span[i]+ " ");
+//         }
+//     }
+// }
+
+import java.util.*;
+//Stock Span Problem
+
+public class Stock_Span_Problem {
+    public static void stockSpan(int stocks[]) {
+        Stack<Integer> s = new Stack<>();
+        int span[] = new int[stocks.length];
+        span[0] = 1;
+        s.push(0);
+        
+        for(int i=1; i<stocks.length; i++) {
+            int curr = stocks[i];
+            while(!s.isEmpty() && curr >= stocks[s.peek()]) {
+                s.pop();
+            }
+            if(s.isEmpty()) {
+                span[i] = i+1;
+            } else {
+                int prevHigh = s.peek();
+                span[i] = i - prevHigh;
+            }
+            
+            s.push(i);
+        }
+
+        for(int i=0; i<span.length; i++) {
+            System.out.print(span[i]+" ");
+        }
+    }
+    public static void main(String args[]) {
+        int stocks[] = {100, 80, 60, 70, 60, 85, 100};
+        stockSpan(stocks);
+    }
+}
